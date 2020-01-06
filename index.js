c694a388bc27834cf91605084e95c1f77e3e183e
Ajar-maharjan <pleiades.ajar@gmail.com>
2020-01-06 13:19:44 +0545

    remake

--- index.js
+++ index.js
@@ -1,136 +1,56 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
-const userController = require("./Controllers/UserController.js");
-const authController = require("./Controllers/AuthController.js");
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-var swaggerDefinition = {
-    info: {
-        title: 'Assignment2_Api',
-        description: 'Register,Login and delete user using token and swagger documentation',
-        version: '1.0.0'
-    },
-    securityDefinitions: {
-        bearerAuth: {
-            type: 'apiKey',
-            name: 'authorization',
-            in: 'header',
-            scheme: 'bearer',
-        }
-    },
-    host: 'localhost:3003',
-    basePath: '/'
-};
-
-var swaggerOptions = {
-    swaggerDefinition,
-    apis: ['./index.js']
-};
-
-var swaggerSpecs = swaggerJSDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+const express= require("express");
+const mongoose = require('mongoose');
+const uploadRouter = require('./routes/upload');
 
 
-/**
- * @swagger
- * /registration:
- *  post:
- *   tags:
- *    - Create users
- *   description: Users registration testing
- *   produces:
- *    - application/json
- *   consumes:
- *    - application/x-www-form-urlencoded
- *   parameters:
- *    - name: username
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique username
- *    - name: password
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique password
- *   responses:
- *    201:
- *     description: user registered successfully
- *    406:
- *     description: username is required or password is required
- *    409:
- *     description: username already exist
- */
 
-/**
- * @swagger
- * /login:
- *  post:
- *   tags:
- *    - Login users
- *   description: Users login testing
- *   produces:
- *    - application/json
- *   consumes:
- *    - application/x-www-form-urlencoded
- *   parameters:
- *    - name: username
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique username
- *    - name: password
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique password
- *   responses:
- *    406:
- *     description: username is required or password is required
- *    404:
- *     description: username doesnt exist
- *    409:
- *     description: incorrect password 
- */
+const app= express();
+app.use('/upload', uploadRouter);
 
-/**
- * @swagger
- * /users/{id}:
- *  delete:
- *   tags:
- *    - Delete users
- *   description: Delete user from token testing
- *   produces:
- *    - application/json
- *   consumes:
- *    - application/x-www-form-urlencoded
- *   security:
- *    - bearerAuth: []
- *   parameters:
- *    - name: id
- *      in: path
- *      required: true
- *      description: please enter id
- *   responses:
- *    401:
- *     description: unauthorized access
- *    404:
- *     description: users not found
- *    200:
- *     description: user deleted successfully
- *    406:
- *     description: Id not provided
- */
 
-app.post('/registration', userController.Validator, userController.UserExist,
-    userController.genHash, userController.Register);
-app.post('/login', authController.validation, authController.passwordChecker,
-    authController.jwtTokenGen);
-app.delete('/users/:id', authController.verifyToken, userController.deleteuser);
-app.listen(3003);
+app.use (express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/Hamrobazar", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+const productSchema = {
+    name:String,
+    price: Number,
+    condition: String,
+    image:{
+       type: String
+      }
+  };
+
+  const Products = mongoose.model("products",productSchema);
+
+
+
+
+  
+  app.route("/products")
+  .get(function(req,res){
+    Products.find(function(err, foundProducts){
+  if(!err){
+    res.send(foundProducts)
+  }else{
+    res.send(err);
+  }
+  
+    });
+  })
+  
+  
+  app.route("/products/:productTitle")
+  .get(function(req, res){
+    Products.findOne({title: req.params.products},function(err, foundProducts){
+      if(foundProducts){
+        res.send(foundProducts);
+      }else{
+        res.send("no products");
+      }
+    });
+  });
+  
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
